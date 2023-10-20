002f61925fc1ed18a967ce22c371393c7cc5fbd3
TheSecondReal0 <66881186+TheSecondReal0@users.noreply.github.com>
2023-10-20 17:34:04 -0600

    added better prompt input

--- addons/godot_gpt/examples/example.gd
+++ addons/godot_gpt/examples/example.gd
@@ -9,17 +9,25 @@ func _ready():
 
 func _on_gpt_request_completed(response_text: String):
 	add_text_to_chat(response_text, "ChatGPT")
-	print(gpt.history)
 
-func _on_button_pressed():
-	gpt.gpt_chat_request(prompt_input.text)
-	add_text_to_chat(prompt_input.text, "Me")
+func send_chat_request() -> void:
+	var prompt: String = prompt_input.text
 	prompt_input.text = ""
-
-func _on_text_edit_text_changed():
-	pass # Replace with function body.
+	
+	# no need to send a request if the prompt is empty
+	if prompt == "":
+		return
+	
+	gpt.gpt_chat_request(prompt)
+	add_text_to_chat(prompt, "Me")
 
 func add_text_to_chat(text: String, from: String) -> void:
 	chat_text.text += from + ": " + text
 	if not chat_text.text.ends_with("\n"):
 		chat_text.text += "\n"
+
+func _on_button_pressed():
+	send_chat_request()
+
+func _on_prompt_submitted() -> void:
+	send_chat_request()
