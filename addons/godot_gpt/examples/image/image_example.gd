f4cad891f9ff93c9f0d509ee4992123fbe735614
TheSecondReal0 <66881186+TheSecondReal0@users.noreply.github.com>
2023-10-20 19:07:58 -0600

    refactored image example

--- addons/godot_gpt/examples/image/image_example.gd
+++ addons/godot_gpt/examples/image/image_example.gd
@@ -1,46 +1,44 @@
 extends Control
 
-@export var img_gen: ImageRequest
-@export var prompt_input: TextEdit
+@export var api_key: String
+
+@export_group("internal")
+@export var image_request: ImageRequest
+@export var texture_rect: TextureRect
+@export var prompt_input: Control
 
 var state_ready: bool = true
 
 func _ready():
-	img_gen = ImageRequest.new()
-	add_child(img_gen)
-	img_gen.api_key = "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"
+	image_request.api_key = api_key
 	
-	#connect signals from img_gen object to functions to be used externally
-	img_gen.image_request_completed.connect(_on_image_request_completed)
-	img_gen.image_request_failed.connect(_on_image_request_failed)
+	#connect signals from image_request object to functions to be used externally
+	image_request.image_request_completed.connect(_on_image_request_completed)
+	image_request.image_request_failed.connect(_on_image_request_failed)
 	
 	var image: Image = Image.new()
 	image = image.load_from_file("res://addons/godot_gpt/examples/image/test.png")
 	var texture: ImageTexture = ImageTexture.new()
 	texture = texture.create_from_image(image)
-	$TextureRect.texture = texture
+	texture_rect.texture = texture
 
 func prompt(prompt: String):
-	print("Prompting")
-	img_gen.image_request(prompt)
+	image_request.image_request(prompt)
 
 func _on_image_request_completed(image: Image):
-	print("Image received")
 	var texture: ImageTexture = ImageTexture.new()
 	texture = texture.create_from_image(image)
-	$TextureRect.texture = texture
+	texture_rect.texture = texture
 	
 	state_ready = true
-	$prompt_input.set_button_state(false)
+	prompt_input.set_button_state(false)
 
 func _on_image_request_failed():
-	state_ready = true
-	$Button.disabled = false
-	$Button.text = "Submit, but last thing errored"
+	prompt_input.set_button_state(false)
 
-func _on_prompt_input_prompt_submitted(prompt):
+func _on_prompt_input_prompt_submitted(prompt: String):
 	if !state_ready:
 		return
 	state_ready = false
-	$prompt_input.set_button_state(true)
+	prompt_input.set_button_state(true)
 	prompt(prompt)
