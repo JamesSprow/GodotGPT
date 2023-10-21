380608ece5ce8d106995cac1101b7045da24a8fd
TheSecondReal0 <66881186+TheSecondReal0@users.noreply.github.com>
2023-10-20 18:52:22 -0600

    minor improvements

--- addons/godot_gpt/examples/common/prompt_input/prompt_input.gd
+++ addons/godot_gpt/examples/common/prompt_input/prompt_input.gd
@@ -5,6 +5,8 @@ extends HBoxContainer
 @onready var text_box: TextEdit = $prompt_input_text
 @onready var button: Button = $submit_button
 
+var loading: bool = false
+
 signal prompt_submitted(prompt: String)
 
 func _ready() -> void:
@@ -12,11 +14,15 @@ func _ready() -> void:
 	button.pressed.connect(submit_prompt)
 
 func submit_prompt() -> void:
+	if loading:
+		return
+	
 	var prompt: String = text_box.text
 	text_box.text = ""
 	prompt_submitted.emit(prompt)
 
-func set_button_state(loading: bool) -> void:
+func set_button_state(_loading: bool) -> void:
+	loading = _loading
 	if loading:
 		button.text = "Loading..."
 		button.disabled = true
