21c4bb8001f7341e0078c3563ed0251e176b4c47
TheSecondReal0 <66881186+TheSecondReal0@users.noreply.github.com>
2023-10-23 12:31:50 -0600

    add web export

--- addons/godot_gpt/examples/chat/chat_entry/chat_entry.gd
+++
@@ -1,8 +0,0 @@
-extends PanelContainer
-
-@export var label_node: Label
-@export var text_node: RichTextLabel
-
-func configure(label: String, text: String) -> void:
-	label_node.text = label
-	text_node.text = text
