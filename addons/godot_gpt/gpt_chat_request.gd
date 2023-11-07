8b96811c0ec4e313c5fbae8a920df4813ca72ceb
TheSecondReal0 <66881186+TheSecondReal0@users.noreply.github.com>
2023-11-06 17:38:12 -0700

    added option to include seed prompt

--- addons/godot_gpt/gpt_chat_request.gd
+++ addons/godot_gpt/gpt_chat_request.gd
@@ -2,8 +2,10 @@
 extends GPTRequest
 # Assign a custom class name "GPTChatRequest" for easier reference and instantiation in other scripts.
 class_name GPTChatRequest
-
 ## This class provides an interface to have a conversation with ChatGPT and use functions in a chat environment.
+
+## Text to feed ChatGPT as its seed. For example "You are a helpful assistant trying to help the user make a Godot game" or "You are a chatbot that can only say 'Bark'"
+@export_multiline var seed_prompt: String = ""
 
 ## Stores the conversation history. Each entry is a dictionary with details about the message.
 ## A potential chat history could look like this:
@@ -43,6 +45,10 @@ func gpt_chat_request(prompt: String) -> Error:
 	# Append the user's message to the list of messages.
 	messages.append(message)
 	
+	# If there is no chat history, insert seed prompt at the beginning
+	if history.is_empty() and not seed_prompt.is_empty():
+		history.append(get_seed_message())
+	
 	# Update the main conversation history with the user's message.
 	history.append(message)
 	
@@ -68,3 +74,9 @@ func gpt_function_respond(function_name: String, response: String) -> Error:
 	
 	# Send the updated list of messages to GPT to generate a completion and return any potential error.
 	return gpt_completions_request(messages)
+
+func get_seed_message() -> Dictionary:
+	return {
+		"role": "system",
+		"content": seed_prompt
+	}
